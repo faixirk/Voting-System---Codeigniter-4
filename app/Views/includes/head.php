e787dbc939cb693de60985511bcd55f7c8a8f4ad
AdnanQadirKhan <adnan.qadir.792@gmail.com>
2022-11-02 11:18:24 +0500

    assets added

--- app/Views/includes/head.php
+++ app/Views/includes/head.php
@@ -1,48 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8"/>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-
-
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="description" content="Prophecy - An Online Betting Platform">
-<meta name="keywords" content="bet, bet365, betting, cricket, gambling, game, hyip, invest, ipl, live, lottery, online betting, online game, soccer, sports">
-<link rel="shortcut icon" href="https://script.bugfinder.net/prophecy/assets/uploads/logo/favicon.png" type="image/x-icon">
-
-<link rel="apple-touch-icon" href="https://script.bugfinder.net/prophecy/assets/uploads/logo/logo.png">
-<title>Prophecy | Home</title>
-<link rel="icon" type="image/png" sizes="16x16" href="https://script.bugfinder.net/prophecy/assets/uploads/logo/favicon.png">
-<meta name="apple-mobile-web-app-capable" content="yes">
-<meta name="apple-mobile-web-app-status-bar-style" content="black">
-<meta name="apple-mobile-web-app-title" content="Prophecy | Home">
-
-<meta itemprop="name" content="Prophecy | Home">
-<meta itemprop="description" content="Prophecy - An Online Betting Platform">
-<meta itemprop="image" content="https://script.bugfinder.net/prophecy/assets/uploads/logo/meta.png600x315">
-
-<meta property="og:type" content="website">
-<meta property="og:title" content="Prophecy - An Online Betting Platform">
-<meta property="og:description" content="Prophecy - An Online Betting Platform">
-<meta property="og:image" content="https://script.bugfinder.net/prophecy/assets/uploads/logo/meta.png"/>
-<meta property="og:image:type" content="image/png"/>
-<meta property="og:url" content="https://script.bugfinder.net/prophecy">
-
-<meta name="twitter:card" content="summary_large_image">
+    <meta name="keywords" content="bet, bet365, betting, cricket, gambling, game, hyip, invest, ipl, live, lottery, online betting, online game, soccer, sports">
+    
+    <link rel="shortcut icon" href="https://script.bugfinder.net/prophecy/assets/uploads/logo/favicon.png" type="image/x-icon">
+    <link rel="apple-touch-icon" href="https://script.bugfinder.net/prophecy/assets/uploads/logo/logo.png">
+    <title><?= $title ?></title>
+    <link rel="icon" type="image/png" sizes="16x16" href="https://script.bugfinder.net/prophecy/assets/uploads/logo/favicon.png">
+    
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta name="apple-mobile-web-app-title" content="Prophecy | Home">
+    <meta itemprop="name" content="Prophecy | Home">
+    <meta itemprop="description" content="Prophecy - An Online Betting Platform">
+    <meta itemprop="image" content="https://script.bugfinder.net/prophecy/assets/uploads/logo/meta.png600x315">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Prophecy - An Online Betting Platform">
+    <meta property="og:description" content="Prophecy - An Online Betting Platform">
+    <meta property="og:image" content="https://script.bugfinder.net/prophecy/assets/uploads/logo/meta.png" />
+    <meta property="og:image:type" content="image/png" />
+    <meta property="og:url" content="https://script.bugfinder.net/prophecy">
+    <meta name="twitter:card" content="summary_large_image">
 
 
-    <link rel="stylesheet" type="text/css" href="https://script.bugfinder.net/prophecy/assets/themes/betting/css/bootstrap.min.css"/>
-    <link rel="stylesheet" type="text/css" href="https://script.bugfinder.net/prophecy/assets/themes/betting/css/skitter.css"/>
-        <link rel="stylesheet" type="text/css" href="https://script.bugfinder.net/prophecy/assets/themes/betting/css/animate.css"/>
-    <link rel="stylesheet" type="text/css" href="https://script.bugfinder.net/prophecy/assets/themes/betting/css/owl.carousel.min.css"/>
-    <link rel="stylesheet" type="text/css" href="https://script.bugfinder.net/prophecy/assets/themes/betting/css/owl.theme.default.min.css"/>
+    <link rel="stylesheet" type="text/css" href="<?= base_url('public/assets/css/bootstrap.min.css') ?>" />
+    <link rel="stylesheet" type="text/css" href="<?= base_url('public/assets/css/skitter.css') ?>" />
+    <link rel="stylesheet" type="text/css" href="<?= base_url('public/assets/css/animate.css') ?>" />
+    <link rel="stylesheet" type="text/css" href="<?= base_url('public/assets/css/owl.carousel.min.css') ?>" />
+    <link rel="stylesheet" type="text/css" href="<?= base_url('public/assets/css/style.css') ?>" />
+    <link rel="stylesheet" type="text/css" href="<?= base_url('public/assets/css/jquery.fancybox.min.css') ?>" />
+    <link rel="stylesheet" type="text/css" href="<?= base_url('public/assets/css/owl.theme.default.min.css') ?>" />
+    <link rel="stylesheet" type="text/css" href="<?= base_url('public/assets/css/aos.css') ?>" />
 
-    <link rel="stylesheet" type="text/css" href="https://script.bugfinder.net/prophecy/assets/themes/betting/css/aos.css"/>
-    <link rel="stylesheet" type="text/css" href="https://script.bugfinder.net/prophecy/assets/themes/betting/css/jquery.fancybox.min.css"/>
+    <script src="<?= base_url('public/assets/js/fontawesomepro.js') ?>"></script>
 
-    <script src="https://script.bugfinder.net/prophecy/assets/admin/js/fontawesome/fontawesomepro.js"></script>
-
-    <link rel="stylesheet" type="text/css" href="https://script.bugfinder.net/prophecy/assets/themes/betting/css/style.css"/>
 
     <style>
         .modal .text-box,
@@ -51,5 +47,5 @@
             background-size: cover;
         }
     </style>
-    
+
 </head>
